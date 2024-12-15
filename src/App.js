3eab59af63bbb04f185a3b867d1e7daad7448251
Hikaru <epichikaru@gmail.com>
2024-12-15 11:09:33 -0800

    Login to SearchForPlayers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './LoginPage/Login.jsx';
 import Game from './Game/Game.jsx';
+import Search from './SearchForPlayer/SearchForPlayer.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/game" element={<Game />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </Router>
   );
