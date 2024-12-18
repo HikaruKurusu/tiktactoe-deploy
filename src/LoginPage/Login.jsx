c1fa65a41ead8ed38d6c432cdaba6b51ac61141f
ricecakes119 <jasonelcano119@gmail.com>
2024-12-17 20:52:55 -0800

    pythonanywhere

--- src/LoginPage/Login.jsx
+++ src/LoginPage/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
 
     try {
       const response = await fetch(
-        `http://127.0.0.1:5000/${endpoint}`,
+        `https://hikarukurusu.pythonanywhere.com/${endpoint}`,
         {
           method: "POST",
           headers: {
